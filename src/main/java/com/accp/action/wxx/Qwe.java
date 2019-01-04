6656af9caf659149cdd0ed2635f348826a244217
wxx2019 <wxx815196612@163.com>
2019-01-04 11:15:36 +0800

    第三次提交boot项目

--- src/main/java/com/accp/action/wxx/Qwe.java
+++ src/main/java/com/accp/action/wxx/Qwe.java
@@ -5,6 +5,8 @@ public class Qwe {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("YYYY");
+		System.out.println("MM");
+		System.out.println("DD");
 	}
 
 }
